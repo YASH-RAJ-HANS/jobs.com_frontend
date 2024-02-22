b460567fda8419174bd75bad771ae64419f81ab5
YASH-RAJ-HANS <yashhans479@gmail.com>
2024-02-22 22:56:12 +0530

    YRH dashboard

--- src/app/Dashboard/page.js
+++ src/app/Dashboard/page.js
@@ -1,12 +1,15 @@
 import JobDetail from "@/components/JobDetail";
 import UserProfile from "@/components/UserProfile";
 import React from "react";
+import Layout from "../appMain/Layout/Layout";
 
 export default function page() {
   return (
-    <div className="w-[80%] m-auto">
+    <div className=" m-auto">
       {/* <JobDetail /> */}
-      <UserProfile />
+      <Layout>
+        <UserProfile />
+      </Layout>
     </div>
   );
 }
